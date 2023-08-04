72c438fe4f65d47db25997025d40ead7648cc3df
Emrer06 <emreramiz06@gmail.com>
2023-08-04 17:37:00 -0400

    changed from manual sizing to using sizers

--- wxWidgetsTest/MainFrame.cpp
+++ wxWidgetsTest/MainFrame.cpp
@@ -2,33 +2,73 @@
 #include <wx/wx.h>
 
 
+
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title)
 {
 	
 	wxPanel* panel = new wxPanel(this);
 
-	wxButton* timeSelectButton = new wxButton(panel, wxID_ANY, "Time Select", wxPoint(120, 60), wxSize(100, 25)); //opens options for reminder time
+	wxButton* timeSelectButton = new wxButton(panel, wxID_ANY, "Time Select", wxDefaultPosition, wxDefaultSize); //opens options for reminder time
+	wxFont buttonFont(wxFontInfo(14)); 
+	timeSelectButton->SetFont(buttonFont);
 
 	wxArrayString tempChoices;
 	tempChoices.Add("Default Audio");
 	tempChoices.Add("Temp A");
 	tempChoices.Add("Temp B");
 	tempChoices.Add("Temp C");
-
-	wxChoice* dropDown = new wxChoice(panel, wxID_ANY, wxPoint(380, 60), wxSize(100, -1), tempChoices); //dropDown menu to select audio
-	dropDown->Select(0);
-
-
-	//wxFont volumeFont(wxFontInfo(12).Family(wxFONTFAMILY_DEFAULT).Bold());
-	//wxStaticText* volume = new wxStaticText(panel, wxID_ANY, "Volume:", wxPoint(156,187)); //volume slider text
-	//volume->SetFont(volumeFont); 
-
-	wxSlider* audioSlider = new wxSlider(panel, wxID_ANY, 50, 0, 100, wxPoint(215, 165), 
-		wxSize(250, -1), wxSL_VALUE_LABEL); //volume slider
-
-	wxPNGHandler* handler = new wxPNGHandler;
-	wxImage::AddHandler(handler);
-	wxStaticBitmap* image = new wxStaticBitmap(panel, wxID_ANY, wxBitmap("volumeIcon2.png", wxBITMAP_TYPE_PNG), wxPoint(153, 165), wxSize(50, 50));
 	
+	wxChoice* dropDown = new wxChoice(panel, wxID_ANY, wxDefaultPosition, wxDefaultSize, tempChoices); //dropDown menu to select audio
+	dropDown->Select(0);
+	dropDown->SetFont(buttonFont);
+
+	wxSlider* audioSlider = new wxSlider(panel, wxID_ANY, 50, 0, 100, wxDefaultPosition,
+		wxDefaultSize, wxSL_VALUE_LABEL); //volume slider
+
+	wxPNGHandler* handler = new wxPNGHandler; //handler for png
+	wxImage::AddHandler(handler);						
+	wxStaticBitmap* image = new wxStaticBitmap(panel, wxID_ANY, wxBitmap("volumeIcon2.png", wxBITMAP_TYPE_PNG), 
+		wxDefaultPosition, wxSize(50, 50)); //placed volumeIcon
+
+	timeSelectButton->Bind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);
+	audioSlider->Bind(wxEVT_SLIDER, &MainFrame::OnSliderChanged, this);
+	dropDown->Bind(wxEVT_CHOICE, &MainFrame::OnDropDownSelect, this);       //Dynamic Event Handling for main menu elements
+
+
+
+
+	// Create the horizontal sizer for the button and drop-down menu
+	wxBoxSizer* buttonDropDownSizer = new wxBoxSizer(wxHORIZONTAL);
+	buttonDropDownSizer->Add(timeSelectButton, 1, wxEXPAND | wxALL, 50); 
+	buttonDropDownSizer->Add(dropDown, 1, wxEXPAND | wxALL, 50); 
+
+	// Create the horizontal sizer for the audio slider and image
+	wxBoxSizer* audioImageSizer = new wxBoxSizer(wxHORIZONTAL);
+	audioImageSizer->Add(image, 0, wxALIGN_CENTER_VERTICAL | wxALL, 20);
+	audioImageSizer->Add(audioSlider, 1, wxEXPAND | wxALL, 20);
+
+	// Create the final vertical sizer to hold both horizontal sizers
+	wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+	mainSizer->Add(buttonDropDownSizer, 0, wxEXPAND | wxTOP, 20);
+	mainSizer->Add(audioImageSizer, 0, wxEXPAND);
+
+	panel->SetSizerAndFit(mainSizer);
 }
 
+
+void MainFrame::OnButtonClicked(wxCommandEvent& evt)
+{
+	wxLogStatus("Button Clicked");
+}
+
+void MainFrame::OnDropDownSelect(wxCommandEvent& evt)
+{
+	
+	wxLogStatus(evt.GetString());
+}
+
+void MainFrame::OnSliderChanged(wxCommandEvent& evt)
+{
+	wxString str = wxString::Format("Slider Value: %d", evt.GetInt());
+	wxLogStatus(str);
+}
--- wxWidgetsTest/MainFrame.h
+++ wxWidgetsTest/MainFrame.h
@@ -6,6 +6,11 @@ class MainFrame : public wxFrame
 public:
 	MainFrame(const wxString& title);
 
+private:
+	void OnButtonClicked(wxCommandEvent& evt);
+	void OnSliderChanged(wxCommandEvent& evt);
+	void OnDropDownSelect(wxCommandEvent& evt);
+	
 
 };
 
